1664aa79022d4fd98b8723c24be952c86ddda7ec
Hasnain Sadid <hasnainsadid@gmail.com>
2024-03-23 12:53:20 +0600

    initial commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,29 @@
+  <template>
+    <vue-dropzone
+      ref="myVueDropzone" 
+      id="dropzone" 
+      :options="dropzoneOptions"
+    />
+  </template>
+
+
+  <script>
+  import vueDropzone from 'dropzone-vue3'
+  
+  export default {
+    components: {
+      vueDropzone,
+    },
+    data () {
+      return {
+        dropzoneOptions: {
+          url: 'https://httpbin.org/post',
+          thumbnailWidth: 150,
+          maxFilesize: 10000,
+          headers: { "My-Awesome-Header": "header value" }
+        }
+      }
+    }
+    
+  }
+</script>
